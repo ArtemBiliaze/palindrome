7a8069c442918aea280090cf44ae5fc77a2b8106
Artem Biliaze <artembiliaze.gmail.com>
2025-09-06 17:32:44 +0300

    Add regexp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const isPalindrome = (str) => {
-    let cleanedStr = str.toLowerCase().split(' ').join('');
-    let reversedStr = cleanedStr.split('').reverse().join('')
-    return cleanedStr === reversedStr;
-}
-
-console.log(isPalindrome('А роза упала на лапу Азора'))
-console.log(isPalindrome('hello'))
+const isPalindrome2 = (str) => {
+  let cleanedStr = str.toLowerCase().replace(/[^a-z0-9а-яё]/gi, "");
+  let reversedStr = "";
+  for (let i = cleanedStr.length - 1; i >= 0; i--) {
+    reversedStr += cleanedStr[i];
+  }
+  return reversedStr === cleanedStr;
+};
+console.log(isPalindrome2("А роза упала на лапу Азора"));
+console.log(isPalindrome2("A man, a plan, a canal: Panama"));
+console.log(isPalindrome2("hello"));
