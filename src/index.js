a5f56030696ed5458d470392edb3d550de9ee6b6
Artem Biliaze <artembiliaze.gmail.com>
2025-09-07 16:25:20 +0300

    refactor: remove unnecessary variable for optimization

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const isPalindrome2 = (str) => {
-  let cleanedStr = str.toLowerCase().replace(/[^a-z0-9а-яё]/gi, "");
-  let reversedStr = "";
-  for (let i = cleanedStr.length - 1; i >= 0; i--) {
-    reversedStr += cleanedStr[i];
+  let cleanedStr = str.toLowerCase().replace(/[^a-z0-9а-яё]/g, "");
+  for (let i = 0; i < cleanedStr.length; i++) {
+     if(!(cleanedStr[(cleanedStr.length - 1) - i] === cleanedStr[i] )) {
+        return false ;
+     }
   }
-  return reversedStr === cleanedStr;
+  return true;
 };
 console.log(isPalindrome2("А роза упала на лапу Азора"));
 console.log(isPalindrome2("A man, a plan, a canal: Panama"));
